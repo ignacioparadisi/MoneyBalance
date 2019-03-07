1183d2e8048c82a35d879253e234f99b1bba972c
Ignacio Paradisi <ignacioparadisi@gmail.com>
2019-03-06 21:20:43 -0400

    Added documentation

--- MoneyBalance/Views/TableViewCell/AccountTableViewCell/AccountTableViewCell.swift
+++ MoneyBalance/Views/TableViewCell/AccountTableViewCell/AccountTableViewCell.swift
@@ -13,14 +13,10 @@ class AccountTableViewCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var moneyLabel: UILabel!
     @IBOutlet weak var currentAccountImageView: UIImageView!
+    var account: Account = Account()
     var isCurrentAccount: Bool = false {
         didSet {
-            if isCurrentAccount {
-                currentAccountImageView.image = UIImage(named: "checked")
-                currentAccountImageView.setImageColor(color: ThemeManager.currentTheme().accentColor)
-            } else {
-                currentAccountImageView.image = nil
-            }
+            setSelectedState()
         }
     }
     
@@ -37,6 +33,16 @@ class AccountTableViewCell: UITableViewCell {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    /// Sets the selected state of the cell
+    private func setSelectedState() {
+        if isCurrentAccount {
+            currentAccountImageView.image = UIImage(named: "checked")
+            currentAccountImageView.setImageColor(color: ThemeManager.currentTheme().accentColor)
+        } else {
+            currentAccountImageView.image = nil
+        }
     }
     
 }
--- MoneyBalance/Views/TableViewCell/DefaultTableViewCell/DefaultTableViewCell.swift
+++ MoneyBalance/Views/TableViewCell/DefaultTableViewCell/DefaultTableViewCell.swift
@@ -16,11 +16,5 @@ class DefaultTableViewCell: UITableViewCell {
         super.awakeFromNib()
 
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
 }
